e014aa270ad6d220f295c0eb306199bdcec9b4ab
vasiliy isaykin <v.chaser@mail.ru>
2025-08-05 06:30:12 +1000

    ...

--- front/lib/ui/screens/token_setup_screen.dart
+++ front/lib/ui/screens/token_setup_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front/features/utils/bloc/safe_bloc.dart';
