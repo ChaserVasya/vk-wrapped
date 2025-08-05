9075cb7651294362a791c2141fe32d4c8b0ae4fe
vasiliy isaykin <v.chaser@mail.ru>
2025-08-05 18:17:02 +1000

    ...

---
+++ front/lib/ui/widgets/empty_state.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class EmptyStateWidget extends StatelessWidget {
+  final String text;
+
+  const EmptyStateWidget({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.inbox_outlined, size: 64, color: Colors.grey[400]),
+          const SizedBox(height: 16),
+          Text(
+            text,
+            style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
